db4911fa74025e2c549d37010da4d62959cd4f71
Niall Kenny <niall.s.kenny@gmail.com>
2019-09-06 17:20:14 +0100

    added authentication middleware.

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcrypt");
 const User = require("../../server/models/user");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
   createUser: async ({ userInput: { email, password } }) => {
@@ -18,5 +19,23 @@ module.exports = {
     } catch (err) {
       throw err;
     }
+  },
+  login: async ({ email, password }) => {
+    // find user by email
+    const user = await User.findOne({ email });
+    if (!user) throw new Error("Email does not exist");
+
+    // check if password is correct
+    const passwordComparison = await bcrypt.compare(password, user.password);
+    if (!passwordComparison) throw new Error("Password is Incorrect");
+
+    const token = jwt.sign(
+      { userId: user._id, email: user.email },
+      "superSecretKey",
+      {
+        expiresIn: "1h"
+      }
+    );
+    return { userId: user._id, token, tokenExpiration: 1 };
   }
 };
--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -38,9 +38,16 @@ module.exports = buildSchema(`
       date: String!
     }
 
+    type AuthData {
+      userId: ID!
+      token: String!
+      tokenExpiration: Int!
+    }
+
     type RootQuery {
         events: [Event!]!
         bookings: [Booking!]!
+        login(email:String!, password:String!): AuthData!
     }
 
     type RootMutation{
